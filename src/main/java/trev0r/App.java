3fc6f4ea07583889158ba7a95f6d22d24b526500
trev0r <trev0r@163.com>
2019-01-11 19:19:01 +0800

    init

---
+++ src/main/java/trev0r/App.java
@@ -0,0 +1,16 @@
+package trev0r;
+
+import org.apache.catalina.startup.Bootstrap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String userDir = System.getProperty("user.dir");
+        System.out.println(userDir);
+    }
+}
